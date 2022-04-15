d9db0cfbf96f7203888dbf2df4a31f7ff03fe7aa
Henry <henrykc24@gmail.com>
2022-04-15 14:40:19 +0100

    Add a deeper function type with a callback funtion

--- appFunction.js
+++ appFunction.js
@@ -2,8 +2,15 @@ var add = function (n1, n2) { return n1 + n2; };
 var printResult = function (num) {
     console.log('Result: ' + num);
 };
+var addAndHandle = function (num1, num2, callBack) {
+    var result = num1 + num2;
+    callBack(result);
+};
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
 printResult(add(4, 12));
 var combinedValues;
 combinedValues = add;
-combinedValues = printResult;
-console.log(combinedValues());
+// combinedValues = printResult;
+console.log(combinedValues(9, 20));
--- appFunction.ts
+++ appFunction.ts
@@ -4,6 +4,15 @@ const printResult = (num: number): void => {
   console.log('Result: ' + num)
 };
 
+const addAndHandle = (num1: number, num2: number, callBack: (num: number) => void) => {
+  const result = num1 + num2;
+  callBack(result);
+};
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
+
 printResult(add(4, 12));
 
 let combinedValues: (num: number, num2: number) => number;
