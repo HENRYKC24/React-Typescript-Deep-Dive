2f994d5437102e936a94709ce8129ed10a585db9
Henry <henrykc24@gmail.com>
2022-04-15 13:25:53 +0100

    Use type aliases

--- appUnion.ts
+++ appUnion.ts
@@ -1,5 +1,6 @@
-const combine = (input1: number | string, input2: number | string) => {
-  let result: number | string;
+type NumStr = number | string;
+const combine = (input1: NumStr, input2: NumStr) => {
+  let result: NumStr;
   if (typeof input1 === 'number' && typeof input2 === 'number') {
     result = input1 + input2;
   } else {
