783cec6506d0818c40369ad5ed2a6f4d46dbb959
Henry <henrykc24@gmail.com>
2022-04-16 08:12:46 +0100

    Learn about readonly in typescript

--- appClass.js
+++ appClass.js
@@ -1,11 +1,12 @@
 ;
 var User = /** @class */ (function () {
     function User(firstName, lastName) {
+        this.nairaSymbol = '₦'; // You can not change a readonly property in the class. You can only read it
         this.firstName = firstName;
         this.lastName = lastName;
     }
     User.prototype.getFullName = function () {
-        return "".concat(this.firstName, " ").concat(this.lastName);
+        return "".concat(this.firstName, " ").concat(this.lastName, " uses the ").concat(this.nairaSymbol, " symbol.");
     };
     User.hoursPerDay = 24; // static properties are accessible outside the class but as a property of the class itself not the instance
     return User;
--- appClass.ts
+++ appClass.ts
@@ -6,6 +6,7 @@ class User implements UserInterface {
   private firstName: string; // can only be used inside this class
   protected lastName: string; // extends to a child class but not public
   static hoursPerDay: number = 24; // static properties are accessible outside the class but as a property of the class itself not the instance
+  readonly nairaSymbol:string = '₦'; // You can not change a readonly property in the class. You can only read it
 
   constructor(firstName: string, lastName: string) {
     this.firstName = firstName;
@@ -13,7 +14,7 @@ class User implements UserInterface {
   }
 
   getFullName(): string {
-    return `${this.firstName} ${this.lastName}`;
+    return `${this.firstName} ${this.lastName} uses the ${this.nairaSymbol} symbol.`;
   }
 }
 
