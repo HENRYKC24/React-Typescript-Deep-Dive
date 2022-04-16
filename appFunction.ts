904cd87521678af3287226c7750b3369b888912e
Henry <henrykc24@gmail.com>
2022-04-16 08:43:53 +0100

    Make changed to ts files and ts.config file

--- appFunction.ts
+++ appFunction.ts
@@ -1,22 +1,22 @@
-const add = (n1: number, n2: number): number => n1 + n2;
+const plus = (n1: number, n2: number): number => n1 + n2;
 
 const printResult = (num: number): void => {
   console.log('Result: ' + num)
 };
 
-const addAndHandle = (num1: number, num2: number, callBack: (num: number) => void) => {
+const plusAndHandle = (num1: number, num2: number, callBack: (num: number) => void) => {
   const result = num1 + num2;
   callBack(result);
 };
 
-addAndHandle(10, 20, (result) => {
+plusAndHandle(10, 20, (result) => {
   console.log(result);
 });
 
-printResult(add(4, 12));
+printResult(plus(4, 12));
 
 let combinedValues: (num: number, num2: number) => number;
-combinedValues = add;
+combinedValues = plus;
 // combinedValues = printResult;
 
 console.log(combinedValues(9, 20));
